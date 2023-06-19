c5badeaec94ca138b9b3d880171e0231ac5a58e7
Michael Hoehn <michael.hoehn@burohappold.com>
2023-06-19 03:27:08 -0400

    123

--- floorPlans/project/public/sketch.js
+++ floorPlans/project/public/sketch.js
@@ -1,87 +1,46 @@
 /// <reference path="../../node_modules/@types/p5/global.d.ts" />
 
-let colorCount = 20;
-let hueValues = [];
-let saturationValues = [];
-let brightnessValues = [];
-let actRandomSeed = 0;
-
 function setup() {
   createCanvas(800, 800);
-  colorMode(HSB, 360, 100, 100, 100);
-  noStroke();
+  background(255);
 }
 
 function draw() {
+  // create a grid of points based on even spacing
+  createGrid(20);
+
+  // select random points and things to draw
+  drawHouse(width / 2, height / 2);
   noLoop();
-  randomSeed(actRandomSeed);
-  let test = $fx.getRawParam("rowCount");
-
-  // colors
-  for (let i = 0; i < colorCount; i++) {
-    if (i % 2 == 0) {
-      hueValues[i] = random(300, 360);
-      saturationValues[i] = 100;
-      brightnessValues[i] = random(15, 100);
-    } else {
-      hueValues[i] = 195;
-      saturationValues[i] = random(20, 100);
-      brightnessValues[i] = 100;
-    }
-  }
-
-  // area tiling
-  // count tiles
-  let counter = 0;
-  let rowCount = int(test);
-  let rowHeight = height / rowCount;
-
-  // seperate each line in parts
-  for (let i = rowCount; i >= 0; i--) {
-    // fragment count
-    let partCount = i + 1;
-    let parts = [];
-
-    for (let ii = 0; ii < partCount; ii++) {
-      if (random() < 0.075) {
-        let fragments = int(random(2, 20));
-        partCount = partCount + fragments;
-        for (let iii = 0; iii < fragments; iii++) {
-          parts.push(random(2));
-        }
-      } else {
-        parts.push(random(2, 20));
-      }
-    }
-
-    // add all subparts
-    let sumPartsTotal = 0;
-    for (let ii = 0; ii < partCount; ii++) {
-      sumPartsTotal += parts[ii];
-    }
-
-    // draw rects
-    let sumPartsNow = 0;
-    for (let ii = 0; ii < parts.length; ii++) {
-      sumPartsNow += parts[ii];
-      let x = map(sumPartsNow, 0, sumPartsTotal, 0, width);
-      let y = rowHeight * i;
-      let w = -map(parts[ii], 0, sumPartsTotal, 0, width);
-      let h = rowHeight;
-      let index = counter % colorCount;
-      let col = color(
-        hueValues[index],
-        saturationValues[index],
-        brightnessValues[index]
-      );
-      fill(col);
-      rect(x, y, w, h);
-      counter++;
-    }
-  }
 }
 
-function mouseReleased() {
-  actRandomSeed = random(100000);
-  loop();
+function drawHouse(x, y) {
+  noFill();
+  strokeWeight(1);
+  beginShape();
+  vertex(x, y);
+  vertex(x + 10, y);
+  vertex(x + 10, y + 10);
+  vertex(x, y + 10);
+  endShape(CLOSE);
+}
+
+function createGrid(n) {
+  //create a grid of points to interact with
+  let points = [
+    {
+      x: this.x,
+      y: this.y,
+    },
+  ];
+
+  // create even columns and rows
+  for (let gridY = 1; gridY < n; gridY++) {
+    for (let gridX = 1; gridX < n; gridX++) {
+      let x = (width / n) * gridX;
+      let y = (height / n) * gridY;
+      points.push({ x, y });
+    }
+  }
+  console.log(points.length + " points have been created");
 }
