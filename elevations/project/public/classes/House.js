743c17e32b361b64588c63071941a666d752fe96
Michael Hoehn <michael.hoehn@burohappold.com>
2023-09-27 22:13:03 -0400

    update to house

--- elevations/project/public/classes/House.js
+++ elevations/project/public/classes/House.js
@@ -1,80 +1,92 @@
 export default class House {
-  constructor(p5, xPos, yPos, scale) {
-    this.type = this.houseType();
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.scale = scale;
-    this.drawHouse(p5, this.type);
-  }
-
-  houseType() {
-    let numHouseTypes = 4;
-    let houseTypeArray = [];
-    for (let i = 0; i < numHouseTypes; i++) {
-      houseTypeArray.push(i);
+    constructor(p5, xPos, yPos, scale) {
+        this.type = this.houseType();
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.scale = scale;
+        this.drawHouse(p5, this.type);
     }
-    return houseTypeArray.at(Math.floor($fx.rand() * houseTypeArray.length));
-  }
 
-  drawHouse(p5, type) {
-    p5.noStroke();
-    switch (type) {
-      case 0:
-        house01(p5, this.xPos, this.yPos, 2);
-        break;
-      case 1:
-        house01(p5, this.xPos, this.yPos, 2);
-        break;
-      case 2:
-        house01(p5, this.xPos, this.yPos, 2);
-        break;
-      case 3:
-        house01(p5, this.xPos, this.yPos, 2);
-        break;
-      default:
-        console.log("Invalid house type: ", type);
-        break;
+    houseType() {
+        let numHouseTypes = 4;
+        let houseTypeArray = [];
+        for (let i = 0; i < numHouseTypes; i++) {
+            houseTypeArray.push(i);
+        }
+        return houseTypeArray.at(Math.floor($fx.rand() * houseTypeArray.length));
     }
-  }
+
+    drawHouse(p5, type) {
+        p5.noStroke();
+        switch (type) {
+            case 0:
+                house01(p5, this.xPos, this.yPos, 2);
+                break;
+            case 1:
+                house01(p5, this.xPos, this.yPos, 2);
+                break;
+            case 2:
+                house01(p5, this.xPos, this.yPos, 2);
+                break;
+            case 3:
+                house01(p5, this.xPos, this.yPos, 2);
+                break;
+            default:
+                console.log("Invalid house type: ", type);
+                break;
+        }
+    }
 }
 
 const house01 = (p5, x, y, scale) => {
-  // to do : integrate scale
-  // scale should derive from grid size
-  p5.stroke("black");
-  p5.strokeWeight(5);
-  p5.noFill();
-  p5.beginShape();
-  if (x > p5.width / 2) {
-    p5.vertex(x, y);
-    p5.vertex(x - 250, y);
-    if (y > p5.height / 2) {
-      p5.fill("yellow");
-      p5.vertex(x - 250, y - 250);
-      p5.vertex(x, y - 250);
-    } else {
-      p5.fill("orange");
-      p5.vertex(x - 250, y + 250);
-      p5.vertex(x, y + 250);
-    }
-    p5.vertex(x, y);
-  } else {
+    // to do : integrate scale
+    // scale should derive from grid size
+    p5.stroke("black");
+    p5.strokeWeight(5);
+    p5.noFill();
     p5.beginShape();
-    p5.vertex(x, y);
-    p5.vertex(x + 250, y);
-    if (y > p5.height / 2) {
-      p5.fill("yellow");
-      p5.vertex(x + 250, y - 250);
-      p5.vertex(x, y - 250);
+    if (x > p5.width / 2) {
+        p5.vertex(x, y);
+        p5.vertex(x - 250, y);
+        if (y > p5.height / 2) {
+            p5.fill("yellow");
+            p5.vertex(x - 250, y - 250);
+            p5.vertex(x, y - 250);
+        } else {
+            p5.fill("orange");
+            p5.vertex(x - 250, y + 250);
+            p5.vertex(x, y + 250);
+        }
+        p5.vertex(x, y);
     } else {
-      p5.fill("orange");
-      p5.vertex(x + 250, y + 250);
-      p5.vertex(x, y + 250);
+        p5.beginShape();
+        p5.vertex(x, y);
+        p5.vertex(x + 250, y);
+        if (y > p5.height / 2) {
+            p5.fill("yellow");
+            p5.vertex(x + 250, y - 250);
+            p5.vertex(x, y - 250);
+        } else {
+            p5.fill("orange");
+            p5.vertex(x + 250, y + 250);
+            p5.vertex(x, y + 250);
+        }
+        p5.vertex(x, y);
     }
-    p5.vertex(x, y);
-  }
-  p5.endShape();
+    p5.endShape();
 };
+
+const house02 = (p5, x, y, scale) => {
+
+}
+
+const house03 = (p5, x, y, scale) => {
+
+}
+
+const house04 = (p5, x, y, scale) => {
+
+}
 
 // Component Selector for House Construction
 
@@ -95,4 +107,4 @@ const house01 = (p5, x, y, scale) => {
 
 // 4. Draw annotations for each point with a random cull
 
-// 5. Draw stree components for each point with a random cull
+// 5. Draw street components for each point with a random cull
