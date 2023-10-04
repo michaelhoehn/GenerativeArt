0accd4f35436b738e7df5b1d8f3b2ece936f6ab0
Michael Hoehn <michael.hoehn@burohappold.com>
2023-10-04 16:28:08 -0400

    Added House02

--- elevations/project/public/classes/DrawingParams.js
+++ elevations/project/public/classes/DrawingParams.js
@@ -1,52 +1,52 @@
 export default class DrawingParams {
-    constructor() {
-        this.aspect = this.aspectRatio(fxrand());
-        this.gridX = this.gridX();
-        this.gridY = this.gridY();
-        this.type = this.gridType();
-        this.scale = this.scale(this.aspect);
-    }
+  constructor() {
+    this.aspect = this.aspectRatio(fxrand());
+    this.gridX = this.gridX();
+    this.gridY = this.gridY();
+    this.type = this.gridType();
+    this.scale = this.scale(this.aspect);
+  }
 
-    aspectRatio(r) {
-        let arW, arH, arT;
-        if (r > 0.66) {
-            arT = "landscape";
-            arW = 4000;
-            arH = 3000;
-        } else if (r > 0.33 && r <= 0.66) {
-            arT = "portrait";
-            arW = 3000;
-            arH = 4000;
-        } else {
-            arT = "square";
-            arW = 4000;
-            arH = 4000;
-        }
-        return [arT, arW, arH];
+  aspectRatio(r) {
+    let arW, arH, arT;
+    if (r > 0.66) {
+      arT = "landscape";
+      arW = 4000;
+      arH = 3000;
+    } else if (r > 0.33 && r <= 0.66) {
+      arT = "portrait";
+      arW = 3000;
+      arH = 4000;
+    } else {
+      arT = "square";
+      arW = 4000;
+      arH = 4000;
     }
+    return [arT, arW, arH];
+  }
 
-    gridX() {
-        return 5;
-    }
+  gridX() {
+    return 20;
+  }
 
-    gridY() {
-        return 5;
-    }
+  gridY() {
+    return 20;
+  }
 
-    gridType() {
-        // replace this with a random selector : return gridType
-        return 2;
-    }
+  gridType() {
+    // replace this with a random selector : return gridType
+    return 2;
+  }
 
-    scale(size) {
-        // replace with scaling logic : return scale
-        // if (size[0] === "landscape") {
-        //     return 1;
-        // } else if (size[0] === "portrait") {
-        //     return 2;
-        // } else {
-        //     return 3;
-        // }
-        return 2;
-    }
+  scale(size) {
+    // replace with scaling logic : return scale
+    // if (size[0] === "landscape") {
+    //     return 1;
+    // } else if (size[0] === "portrait") {
+    //     return 2;
+    // } else {
+    //     return 3;
+    // }
+    return 1;
+  }
 }
